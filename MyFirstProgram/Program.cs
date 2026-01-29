29069ceeddb7f1951b51102e52dd0426e25301c5
AiJared <ojared511@gmail.com>
2026-01-29 08:23:02 +0300

    C sharp basics done

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Why Learn C#?
+/* 
+1. It is flexible for building different kinds of applications from console
+apps, web services, desktop apps and games.
+2. It is the best in the gaming industry, if you want to be a game developer,
+"which in my case I do" then C# and C++ are the right languages for it.
+ */
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is amazing!");
+Console.Beep();
+
+// A couple of things you need to know regarding C# code
+/*
+- A C# program has several layers to it and unlike Python, here it entails a lot
+of typing just like C and Java.
+- Variable names are defined explicitly.
+- All program lines MUST end with a semicolon.
+- There is this Main method that is the entry point of a C# program and immediately
+after defining it, there are curly braces that contain the body of the main method.
+- Everything that is created ontop of the Main methon will be executed when you run
+the code and work down executing any code that follows.
+- In order for any program to run, the Main method MUST be called.
+- There is a Console.WriteLine method that displays whatever text you put into it
+on a console display. As seen from the two examples above, when we run the program,
+it will display Hello, World! and This is amazing! respectfully on the console. The
+text being displayed on the console MUST be written in double quotes.
+ */
