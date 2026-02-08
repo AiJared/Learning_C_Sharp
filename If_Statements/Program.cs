c239419ac31467003fe5200eba8146f9a03d9a4b
AiJared <ojared511@gmail.com>
2026-02-08 15:37:16 +0300

    If statements in C#

---
+++ If_Statements/Program.cs
@@ -0,0 +1,17 @@
+﻿// If statements are the most basic form of decision making
+/*
+This is a condition that must be made for a specific block of code to be executed.
+If not it will either be ignored or something else will be executed.
+*/
+
+Console.WriteLine("Please enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.WriteLine("You are qualified to sign up!");
+}
+else
+{
+    Console.WriteLine("You must be 18+ years old to sign up!");
+}
