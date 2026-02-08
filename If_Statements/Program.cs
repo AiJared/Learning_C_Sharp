2b698dd966e9303f85c16a008c9e224859b6b703
AiJared <ojared511@gmail.com>
2026-02-08 15:46:54 +0300

    if statements including else if and else

--- If_Statements/Program.cs
+++ If_Statements/Program.cs
@@ -1,17 +1,30 @@
 ﻿// If statements are the most basic form of decision making
 /*
 This is a condition that must be made for a specific block of code to be executed.
-If not it will either be ignored or something else will be executed.
+If not it will either be ignored or something else will be executed using an
+else if block and if not we execute the else block.
 */
 
 Console.WriteLine("Please enter your age: ");
 int age = Convert.ToInt32(Console.ReadLine());
 
-if (age >= 18)
+if (age > 100)
+{
+    Console.WriteLine("You are too old to sign up!");
+}
+
+
+else if (age >= 18)
 {
     Console.WriteLine("You are qualified to sign up!");
 }
+
+else if (age <0 )
+{
+    Console.WriteLine("You haven't been boran yet!");
+}
+
 else
 {
-    Console.WriteLine("You must be 18+ years old to sign up!");
+    Console.WriteLine("You must be 18+ to sign up!");
 }
