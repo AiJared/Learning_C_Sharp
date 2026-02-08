ecf668f36bbdcb6ae739b86262609d1bac1dbdf8
AiJared <ojared511@gmail.com>
2026-02-08 11:41:25 +0300

    Converting the entier string to upper case

--- StringMethods/Program.cs
+++ StringMethods/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// String Methods in C#
+/*
+There are a lot of methods we can work with strings so we will go ahead and
+start with converting a string to upper case. We use ToUpper().
+*/
+
+String fullname = "Jared Maina";
+Console.WriteLine(fullname.ToUpper());
