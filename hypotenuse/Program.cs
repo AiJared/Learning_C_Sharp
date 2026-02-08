6cfa54fb4a7a9ab517c10359695e3f972c2395dc
AiJared <ojared511@gmail.com>
2026-02-08 11:34:15 +0300

    Practice program for finding the hypotenuse of a right triange

--- hypotenuse/Program.cs
+++ hypotenuse/Program.cs
@@ -1,1 +1,10 @@
 ﻿// In this small program we will find the hypotenuse of a right triangle.
+Console.WriteLine("Enter side A: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter side B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double c = Math.Sqrt(a * a + b * b);
+
+Console.WriteLine(c);
