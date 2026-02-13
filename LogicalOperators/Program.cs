335d1631b89a1f9ba776bca971262dbd3355491a
AiJared <ojared511@gmail.com>
2026-02-13 09:00:41 +0300

    And operator

--- LogicalOperators/Program.cs
+++ LogicalOperators/Program.cs
@@ -4,3 +4,22 @@ They are used to check if more than one conditions are true.
 
 In this case we will use AND (&&), and OR (||).
 */
+
+Console.WriteLine("What is the temperatur outside: (c)");
+
+Double temp = Convert.ToDouble(Console.ReadLine());
+
+// Let's start with logical AND
+/*
+For this operator to work, both conditions MUST be true. 
+*/
+
+if (temp > 10 && temp <= 25)
+{
+    Console.WriteLine("It's warm outside");
+}
+
+// Now the OR operator
+/*
+For OR operator to work, one of the conditions have to be true. 
+*/
