11cc66cec241c832541056ed032f5ca88be35536
AiJared <ojared511@gmail.com>
2026-02-23 16:28:09 +0300

    while loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿// While Loop
+/*
+While loop repeats a specific code if some condition remains to be true.
+In the example below we will ask a user to enter their name.
+While loop will help us check whether or not they've entered a name.
+If they don't enter anything, it will prompt them to enter it again.
+*/
+Console.WriteLine("Please enter your name: ");
+String name = Console.ReadLine();
+
+while (name == "")
+{
+    Console.WriteLine("Please enter your name: ");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine("Hello " + name);
