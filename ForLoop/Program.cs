95ea7f86ec8c312f5a198ae71d20b3208915fb88
AiJared <ojared511@gmail.com>
2026-02-23 16:40:19 +0300

    for loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿// For Loop
+/*
+This repeats a code a finite amount of times unlike while loop which repeats
+infinitely unless the condition is false.
+In an example below we will write a program that counts from 1 to 10.
+To pull this off we will use for loop which works by declaring three statements.
+The first one is the index we are starting with for example i = 0.
+The second statement is the condition itself for example 1 <= 10.
+The third statement is the number of times we will incrementing the index.
+*/
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
